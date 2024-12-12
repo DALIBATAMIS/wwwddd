3e8d7988b6c4bc7988db1afdc2ee8b453da261bb
DATAWATER <>
2024-12-13 00:31:37 +0400

    feat: pinkandpink wip

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-server_addr = ("0.0.0.0", 8080)
+import socket
+
+server_addr = ('', 8080)
 
 class RedirectHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -11,7 +13,7 @@ class RedirectHandler(SimpleHTTPRequestHandler):
 
 def main():
     server = HTTPServer(server_addr, RedirectHandler)
-    print(f"[INFO] server started at http://{server_addr[0]}:{server_addr[1]}")
+    print(f"[INFO] server started at http://localhost:{server_addr[1]}")
     server.serve_forever()
 
 if __name__ == "__main__":
